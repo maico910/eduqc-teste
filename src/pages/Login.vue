f1ef6f816c4f87d45ab2e82b5328771d42b8a876
Michael Wu <maico910@gmail.com>
2021-08-21 11:21:16 -0300

    add: Login Page

---
+++ src/pages/Login.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title
+          class="teste"
+          bla="ax"
+        >
+          EduQC Teste
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <q-page
+        class="login-page bg-grey-4"
+        padding
+      >
+        <q-card class="q-pa-md">
+          <q-form
+            class="login-form"
+            @submit.prevent="login"
+          >
+            <q-input
+              v-model="user.login"
+              filled
+              label="Login"
+              color="primary"
+            />
+
+            <q-input
+              v-model="user.password"
+              filled
+              label="Senha"
+              color="primary"
+              type="password"
+            />
+
+            <q-btn
+              type="submit"
+              label="Login"
+              color="primary"
+            />
+          </q-form>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        login: '',
+        password: ''
+      }
+    }
+  },
+
+  methods: {
+    login() {
+      if (this.user.login === 'teste@teste.com' && this.user.password === '12345678') {
+        this.$router.push({name: 'index'})
+      }
+
+      this.$q.notify({
+        message: 'Autenticação negado',
+        color: 'negative'
+      })
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  .login-form > *:not(:last-child) {
+    margin-bottom: 16px
+  }
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,8 +4,13 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', name: 'index', component: () => import('pages/Index.vue') }
     ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('pages/Login.vue'),
   },
 
   // Always leave this as last one,
